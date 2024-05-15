217a731798c29c70008c5825ede089356bba6171
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-05-15 15:15:44 +0530

    doctors.js

---
+++ backend/Routes/doctor.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { updateDoctor, deleteDoctor, getAllDoctor, getSingleDoctor, getDoctorProfile} from "../Controllers/doctorController.js";
+//import { authenticate, restrict } from "../auth/verifyToken.js";
+/*import reviewRouter from "./review.js";*/
+
+const router = express.Router();
+
+// Nested route for reviews
+/*router.use("/:doctorId/reviews", reviewRouter);*/
+
+// Get single doctor by ID
+router.get('/:id', getSingleDoctor);
+
+// // Get doctor profile
+// router.get('/profile/me', authenticate, restrict(['doctor']), getDoctorProfile);
+
+// // Get all doctors
+
+router.get('/', getAllDoctor);
+
+// // Update doctor by ID
+// router.put('/:id', authenticate, restrict(["doctor"]), updateDoctor);
+
+// // Delete doctor by ID
+// router.delete('/:id', authenticate, restrict(["doctor"]), deleteDoctor);
+
+export default router;
+
--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ import cors from 'cors';
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import authRoute from "./Routes/auth.js";
-import userRoute from "./Routes/user.js"; 
+import userRoute from "./Routes/user.js";
+import doctorRoute from "./Routes/doctor.js";
+
 dotenv.config();
 
 const app = express();
@@ -38,7 +40,7 @@ app.use(cookieparser());
 app.use(cors(corsOptions));
 app.use('/api/v1/auth', authRoute); // domain/api/v1/auth/register
 app.use('/api/v1/users', userRoute);
-// app.use('/api/v1/doctors', doctorRoute);
+app.use('/api/v1/doctors', doctorRoute);
 // app.use(errorHandler); // Ensure this is an ES Module import
 
 app.listen(port, () => {
